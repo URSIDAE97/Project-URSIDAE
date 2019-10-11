f8b972c6eee22877ff511e337d080506654d7316
URSIDAE 97 <mateusz.m97@gmail.com>
2019-10-11 21:44:58 +0200

    face - tray icon demo

--- face/src/background.js
+++ face/src/background.js
@@ -1,41 +1,22 @@
-'use strict'
+const electron = require('electron')
+const path = require('path')
 
-import { app, protocol, BrowserWindow } from 'electron'
-import {
-  createProtocol,
-  installVueDevtools
-} from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const app = electron.app
+const protocol = electron.protocol
+const BrowserWindow = electron.BrowserWindow
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+const Tray = electron.Tray
+const trayIcon = path.join(__dirname, '../src/assets/logos/logo_1.png')
+const Menu = electron.Menu
 
-// Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }])
+let win = null
+let tray = null
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
-    nodeIntegration: true
-  } })
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
+protocol.registerSchemesAsPrivileged(
+  [{scheme: 'app', privileges: { secure: true, standard: true } }]
+)
 
-  win.on('closed', () => {
-    win = null
-  })
-}
-
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
@@ -48,42 +29,87 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    //createWindow()
   }
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    // Devtools extensions are broken in Electron 6.0.0 and greater
-    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
-    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
-    // If you are not using Windows 10 dark mode, you may uncomment these lines
-    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-    // try {
-    //   await installVueDevtools()
-    // } catch (e) {
-    //   console.error('Vue Devtools failed to install:', e.toString())
-    // }
+app.on('ready', function() {
+  win = new BrowserWindow({ 
+    width: 800, 
+    height: 600, 
+    webPreferences: {
+      nodeIntegration: true
+    }    
+  })
 
-  }
-  createWindow()
+  win.hide();
+  // if (process.env.WEBPACK_DEV_SERVER_URL) {
+  //   // Load the url of the dev server if in development mode
+  //   win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+  //   if (!process.env.IS_TEST) win.webContents.openDevTools()
+  // } else {
+  //   createProtocol('app')
+  //   // Load the index.html when not in development
+  //   win.loadURL('app://./index.html')
+  // }
+
+  win.on('closed', () => {
+    win = null
+  })
+  tray = new Tray(trayIcon)
+
+  let template = [
+    {
+      label: 'Open app'
+    }
+  ]
+
+  const ctxMenu = Menu.buildFromTemplate(template)
+  tray.setContextMenu(ctxMenu)
+  tray.setToolTip('URSIDAE')
+
+
+  tray.on('double-click', function() {
+    win.show()
+  })
 })
 
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', data => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
-    })
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
-  }
+// // Exit cleanly on request from parent process in development mode.
+// if (isDevelopment) {
+//   if (process.platform === 'win32') {
+//     process.on('message', data => {
+//       if (data === 'graceful-exit') {
+//         app.quit()
+//       }
+//     })
+//   } else {
+//     process.on('SIGTERM', () => {
+//       app.quit()
+//     })
+//   }
+// }
+
+function createWindow () {
+  win = new BrowserWindow({ 
+    width: 800, 
+    height: 600, 
+    webPreferences: {
+      nodeIntegration: true
+    }    
+  })
+
+  win.hide();
+  // if (process.env.WEBPACK_DEV_SERVER_URL) {
+  //   // Load the url of the dev server if in development mode
+  //   win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+  //   if (!process.env.IS_TEST) win.webContents.openDevTools()
+  // } else {
+  //   createProtocol('app')
+  //   // Load the index.html when not in development
+  //   win.loadURL('app://./index.html')
+  // }
+
+  win.on('closed', () => {
+    win = null
+  })
 }
