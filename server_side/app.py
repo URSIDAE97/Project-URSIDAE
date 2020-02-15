f21908a062e083de89c10fca62256422f020fb9c
URSIDAE <mateusz.m97@gmail.com>
2020-02-15 19:29:25 +0100

    server quick fix

--- server_side/app.py
+++ server_side/app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template
 
 from api import *
-from services import security
+# from services import security
 from data import database
 
 app = Flask(__name__)
 app.register_blueprint(test_api)
 
 database.initialize_memory(app)
-jwt = security.initialize_security(app)
+# jwt = security.initialize_security(app)
 
 
 @app.route('/', methods=['GET'])
 def info_page():
-    return render_template('index.html'), 401
+    return render_template('index.html'), 200
 
 
-@app.errorhandler(404)
-def route_not_found(msg):
-    return security.unauthorized_redirect(msg)
+# @app.errorhandler(404)
+# def route_not_found(msg):
+#     return security.unauthorized_redirect(msg)
 
 
 if __name__ == '__main__':
